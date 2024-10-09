c97bad380a99b1b3bf1b3591e9031d28454870e9
Gabriel <gabriel.souzagraciano@hotmail.com>
2024-10-09 09:12:13 -0300

    Update day12.py
    added game loop

--- Day12/day12.py
+++ Day12/day12.py
@@ -18,12 +18,22 @@ difficulty = input("Choose the difficulty, type 'easy' or 'hard': ").lower()
 while difficulty != 'easy' and difficulty != 'hard':
     input("Choose the difficulty, type 'easy' or 'hard': ").lower()
 if difficulty == 'easy':
-    lives = 10
+    hearts = 10
 elif difficulty == 'hard':
-    lives = 5
+    hearts = 5
 the_number = randint(1, 100)
 guess = -1
-while guess != the_number and lives > 0:
+while guess != the_number and hearts > 0:
     guess = int(input("Make a guess:  "))
+    if guess > the_number:
+        print("Too high.")
+    if guess < the_number:
+        print("Too low.")
+    if guess != the_number:
+        hearts -= 1
 
+if guess == the_number:
+    print(f"\nYou got it! The answer was {the_number}.\n\n\n")
+else:
+    print("\nYou've run out of guesses, you lose.\n\n\n")
 
