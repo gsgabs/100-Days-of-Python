66f4dea93beca7848c3ceb166c999fe73be455e0
Gabriel <gabriel.souzagraciano@hotmail.com>
2024-10-14 11:51:21 -0300

    Update day14.py
    added firts inputs for user

--- Day14/day14.py
+++ Day14/day14.py
@@ -32,17 +32,23 @@ def generate_random_game():
     return games[index]
 
 
+print("Welcome to HigherVsLower game")
+print("You'll have to guess which from the two presented games was released earlier.")
+print("Let's get started!\n")
+
 game_1 = generate_random_game()
 game_2 = generate_random_game()
 
 while game_1["year"] == game_2["year"] or game_1["name"] == game_2["name"]:
     print(game_1, game_2)
     game_2 = generate_random_game()
-print(game_1, game_2)
 
+print(f"Which was released earlier: {game_1['name']} or {game_2['name']}")
+guess = input(f"Type 1 for {game_1['name']} or 2 for {game_2['name']}: ")
 
-
-
+while (guess != '1' and guess != '2') and (guess != game_1['name'] and guess != game_2['name']):
+    print(f"Type 1 for {game_1['name']} or 2 for {game_2['name']}. ")
+    guess = input("You can also type the game name: ")
 
 
 
